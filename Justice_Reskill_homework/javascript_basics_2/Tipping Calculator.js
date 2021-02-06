7608b4ac13d389e6487fcfa1117fcadbeb67639d
Chad Jay Harrington <MistahWrite@gmail.com>
2021-02-05 18:10:43 -0700

    Completed Tip Calculator

--- Justice_Reskill_homework/javascript_basics_2/Tipping Calculator.js
+++ Justice_Reskill_homework/javascript_basics_2/Tipping Calculator.js
@@ -1,13 +1,15 @@
 //Tipping Calculator
 function TippingCalculator(meal, tip_percent) {
+    console.log("Meal: " + meal + " + Tip: " + tip_percent + "%");
     var tip = meal * (tip_percent * 0.01);
-    if (tip_percent < 5) {
-        console.log(“Less than 5%: 'You are an awful tipper'”);
+
+    if (tip_percent <= 5) {
+        console.log("Less than 5%: 'You are an awful tipper'");
     } else if (tip_percent >= 5 && tip_percent <= 10) {
-        console.log("Between 5% and 10%: 'You are an ok tipper!'”);
-    } else if (tip_percent >= 10 && tip_percent <= 20)
-        console.log("Between 10% and 20%: 'You are a good tipper!'”);
-    } else if (tip_percent >= 20) }
+        console.log("Between 5% and 10%: 'You are an ok tipper!'");
+    } else if (tip_percent >= 10 && tip_percent <= 20) {
+        console.log("Between 10% and 20%: 'You are a good tipper!'");
+    } else if (tip_percent >= 20) {
         console.log("Greater than 20%: 'You are a great tipper!'");
     }
     return meal + tip;
